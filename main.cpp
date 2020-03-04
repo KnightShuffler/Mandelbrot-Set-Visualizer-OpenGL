4c1a06633dcf5bff621ac2801cb0f9be5cd680c9
Swagat Kumar <swagat.ks.97@gmail.com>
2020-03-03 16:40:52 -0800

    Tested window creation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-#include <SDL2/SDL.h>
-#include <GL/glew.h>
+#include <string>
+
+#include "display.h"
+
+#define WINDOW_WIDTH  640
+#define WINDOW_HEIGHT 480
 
 int main (int argc, char** argv) {
-	std::cout << "Hello World!" << std::endl;
+	try {
+		// Create the display object
+		Display d (WINDOW_WIDTH, WINDOW_HEIGHT, "Mandelbrot Set");
+		
+		// Exit loop
+		std::cout << "Enter any key to exit..." << std::endl;
+		int exit;
+		while (std::cin >> exit);
 
-	return 0;
+		return 0;
+	}
+	catch (std::string & ex) {
+		std::cerr << ex << std::endl;
+		exit (EXIT_FAILURE);
+	}
+	catch (...) {
+		std::cerr << "Unexpected behavior, stopping execution" << std::endl;
+		exit (EXIT_FAILURE);
+	}
 }
