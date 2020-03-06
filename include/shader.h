63033e3e1f702061ba6dcde924ff9c5e51c95c36
Swagat Kumar <swagat.ks.97@gmail.com>
2020-03-05 22:11:28 -0800

    Added uniform setting methods to shader class

--- include/shader.h
+++ include/shader.h
@@ -12,6 +12,14 @@ public:
 
 	// Make the GPU use this shader program
 	void bind ();
+
+	// Setting uniforms
+	void setFloat (const std::string& name, float value) const {
+		glUniform1f (glGetUniformLocation (m_program, name.c_str ()), (float)value);
+	}
+	void setFloat (const std::string& name, float v1, float v2) const {
+		glUniform2f (glGetUniformLocation (m_program, name.c_str ()), (float)v1, (float)v2);
+	}
 private:
 	enum { 
 		VERTEX_SHADER = 0u, 
